06216976f8957747d1eb72dc04b744951e0bdbb9
Shaufi <shaufi@polimas.edu.my>
2023-08-15 16:53:51 +0800

    ubah iotcloud ubah code kali kedua

--- iot.ino
+++ iot.ino
@@ -15,7 +15,7 @@
 #include <WiFi.h - master punya>
 #include <WiFiClient.h- tambah apa2>
 #include <BlynkSimpleEsp32.h>
-char auth[] = BLYNK_AUTH_TOKEN;
+char auth[] = BLYNK_AUTH_TOKENabcakdkadjkaj;
 
 char ssid[] = "GRANADA";
 char pass[] = "";
